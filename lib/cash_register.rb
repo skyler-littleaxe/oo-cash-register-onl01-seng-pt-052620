9890c742629735389b392b9a04f7d71ce977bfa8
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-29 02:37:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,6 +8,7 @@ class CashRegister
   end
   
   def add_item (title, price, quantity=1)
+<<<<<<< HEAD
     #self.total += price * quantity
     #@items.fill(title, @items.size, quantity)
     quantity.times do 
@@ -15,15 +16,25 @@ class CashRegister
     end
     @last_total = @total
     @total += price * quantity
+=======
+    self.total += price * quantity
+    @items.fill(title, @items.size, quantity)
+>>>>>>> e18fead5353c6084253a39188c7bf1ec019264c7
   end
   
   def apply_discount
     if @discount == 0
       p "There is no discount to apply."
     else
+<<<<<<< HEAD
       @total = (@total *((100 - @discount) / 100.to_f)).to_i
       #@total = @total - (@discount / 100 * @total) 
       p "After the discount, the total comes to $#{@total}."
+=======
+      #@total = @total *((100 - @discount) / 100)
+      @total = @total - (@discount / 100 * @total) 
+      p "After the discount, your total comes out to $#{@total}."
+>>>>>>> e18fead5353c6084253a39188c7bf1ec019264c7
     end
   end
   
@@ -31,8 +42,11 @@ class CashRegister
     @items
   end
   
+<<<<<<< HEAD
   def void_last_transaction
     @total = @last_total
   end
   
+=======
+>>>>>>> e18fead5353c6084253a39188c7bf1ec019264c7
 end
